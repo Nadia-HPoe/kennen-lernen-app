90fb696629083974ea5bda19f7225303dd23f1db
Nadia-HPoe <isslandia@mail.ru>
2023-10-24 19:33:01 +0300

    adding buttons

--- src/presentation/Header/index.jsx
+++ src/presentation/Header/index.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import './header.scss';
 import Logo from './images/kl_app_logo_header.png';
+import LogIn from './images/kp_app_icon_login.png';
 
 function Header() {
   return (
     <header className="navbar">
       <img src={Logo} alt="logotype" className="header-logo" />
-      <button>Log In</button>
-      <button>Sign Up</button>
-      <button>About</button>
+      <a className="nav-item" href="">
+        <img src={LogIn} alt="login_icon" className="header-icon" />
+        Log In
+      </a>
+      <a className="nav-item" href="">
+        Sign Up
+      </a>
+      <a className="nav-item" href="">
+        About
+      </a>
     </header>
   );
 }
--- src/presentation/Main/Card.jsx
+++ src/presentation/Main/Card.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { words } from '../components/wordlist';
+import { SaveImg } from './images/square-check-solid.svg';
+import { EditImg } from './images/pencil-solid.svg';
+import { DeleteImg } from './images/trash-solid.svg';
 
 function Card() {
   return (
     <table className="table-card" border={1}>
       <thead>
         <tr>
-          <th className="table-col">#</th>
+          <th className="table-col-0">#</th>
           <th className="table-col">Слово на немецком</th>
           <th className="table-col">Перевод</th>
           <th className="table-col"></th>
@@ -19,6 +22,18 @@ function Card() {
               <th>{1 + id}</th>
               <th>{word.german}</th>
               <th>{word.russian}</th>
+              <th>
+                <button className="table-save">
+                  <img src={SaveImg} alt="save" />
+                  Save
+                </button>
+                <button className="table-edit">
+                  <img src={EditImg} alt="edit" />
+                </button>
+                <button className="table-delete">
+                  <img src={DeleteImg} alt="delete" />
+                </button>
+              </th>
             </tr>
           );
         })}
